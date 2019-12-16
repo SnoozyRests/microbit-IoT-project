6f87a1a81da51f85a97260db96e5b291fee2a4ad
ld2-murray <luke4.murray@live.uwe.ac.uk>
2019-12-16 17:39:40 +0000

    pincode start up

--- build/bbc-microbit-classic-gcc/yotta_config.h
+++ build/bbc-microbit-classic-gcc/yotta_config.h
@@ -3,7 +3,7 @@
 // yotta config data (including backwards-compatible definitions)
 #define TARGET BBC_MICROBIT_CLASSIC_GCC
 #define TARGET_LIKE_BBC_MICROBIT_CLASSIC_GCC
-#define YOTTA_BUILD_INFO_HEADER "/home/jacob/iots/iots_task3/build/bbc-microbit-classic-gcc/yotta_build_info.h"
+#define YOTTA_BUILD_INFO_HEADER "/home/student/Documents/iots_task3/build/bbc-microbit-classic-gcc/yotta_build_info.h"
 #define TARGET_LIKE_BBC_MICROBIT_CLASSIC_GCC
 #define TARGET_LIKE_NRF51822
 #define TARGET_LIKE_NRF
