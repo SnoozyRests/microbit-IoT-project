23eb44de0ca7ceeaf04b0e22c27227ae2ede97d5
Jacob Williams <jacob3.williams@live.uwe.ac.uk>
2019-12-31 11:42:00 +0000

    prep

--- build/bbc-microbit-classic-gcc/yotta_build_info.h
+++ build/bbc-microbit-classic-gcc/yotta_build_info.h
@@ -5,10 +5,10 @@
 #define YOTTA_BUILD_MONTH 12 // UTC month 1-12
 #define YOTTA_BUILD_DAY 31 // UTC day 1-31
 #define YOTTA_BUILD_HOUR 11 // UTC hour 0-24
-#define YOTTA_BUILD_MINUTE 1 // UTC minute 0-59
-#define YOTTA_BUILD_SECOND 4 // UTC second 0-61
-#define YOTTA_BUILD_UUID d9edd39f-9361-4f04-ac56-487f528f502a // unique random UUID for each build
-#define YOTTA_BUILD_VCS_ID d251e8d17e277a687e8317ac2aaba38cb76a82f2 // git or mercurial hash
-#define YOTTA_BUILD_VCS_CLEAN 0 // evaluates true if the version control system was clean, otherwise false
-#define YOTTA_BUILD_VCS_DESCRIPTION d251e8d // git describe or mercurial equivalent
+#define YOTTA_BUILD_MINUTE 31 // UTC minute 0-59
+#define YOTTA_BUILD_SECOND 0 // UTC second 0-61
+#define YOTTA_BUILD_UUID 4a8adbda-c16b-4576-afe2-c9d17a7c7075 // unique random UUID for each build
+#define YOTTA_BUILD_VCS_ID 0fbae12259ef36aa0f20e1fda94d15a38b1ce8d5 // git or mercurial hash
+#define YOTTA_BUILD_VCS_CLEAN 1 // evaluates true if the version control system was clean, otherwise false
+#define YOTTA_BUILD_VCS_DESCRIPTION 0fbae12 // git describe or mercurial equivalent
 #endif // ndef __YOTTA_BUILD_INFO_H__
